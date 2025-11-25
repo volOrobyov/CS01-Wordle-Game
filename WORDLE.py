56ae9ec30c04a36d435266661d0883cdf8c9192c
volOrobyov <25fmif.v.orobiov@std.udu.edu.ua>
2025-11-25 20:19:37 +0200

    mplemented visual feedback

--- WORDLE.py
+++ WORDLE.py
@@ -1,11 +1,9 @@
-import colorama
-from colorama import Fore, Back, Style
-
+from colorama import Style, Fore
 import random
+
 print("Ласкаво просимо в гру!")
 print("У вас є 6 спроб щоб вгадати слово")
 words = ["apple","house","water","green","happy","table","light","bread","fruit","sleep"]
-check = []
 secret = random.choice(words)
 attempts = 0
 while attempts < 6:
@@ -18,15 +16,15 @@ while attempts < 6:
             print("Ви ввели слово, яке складається більше або менше ніж з 5 літер")
             continue
         attempts = attempts + 1
+        check = []
         for index in range(5):
             if input_attempt[index] == secret[index]:
-                check.append(f"{input_attempt[index]}-Green")
+                check.append(Fore.GREEN + input_attempt[index] + Style.RESET_ALL)
             elif input_attempt[index] in secret:
-                check.append(f"{input_attempt[index]}-Yellow")
+                check.append(Fore.YELLOW + input_attempt[index] + Style.RESET_ALL)
             else:
-                check.append(f"{input_attempt[index]}-NoMach")
-        check.append("---")
-        print(check)
+                check.append(Fore.RED + input_attempt[index] + Style.RESET_ALL)
+        print(' '.join(check))
         if input_attempt == secret:
             print("Вітаю, ви вгадали")
             print(f"Кількість використаних спроб: {attempts}")
