4ccd8917293076441f4116058d293c63a2a0157d
volOrobyov <25fmif.v.orobiov@std.udu.edu.ua>
2025-11-25 20:35:13 +0200

    Added comments again

--- WORDLE.py
+++ WORDLE.py
@@ -1,7 +1,7 @@
 from colorama import Style, Fore
 import random
 
-print("Ласкаво просимо в гру!")
+print("---ЛАСКАВО ПРОСИМО У ГРУ WORDLE---!")
 print("У вас є 6 спроб щоб вгадати слово")
 words = ["apple","house","water","green","happy","table","light","bread","fruit","sleep"]
 secret = random.choice(words)
@@ -16,14 +16,19 @@ while attempts < 6:
             print("Ви ввели слово, яке складається більше або менше ніж з 5 літер")
             continue
         attempts = attempts + 1
-        check = []
+        check = ["ВВЕДЕНЕ СЛОВО:"]
         for index in range(5):
             if input_attempt[index] == secret[index]:
                 check.append(Fore.GREEN + input_attempt[index] + Style.RESET_ALL)
+                comment = (Fore.LIGHTGREEN_EX + f"{input_attempt[index]} Стоїть на своєму місті" + Style.RESET_ALL)
             elif input_attempt[index] in secret:
                 check.append(Fore.YELLOW + input_attempt[index] + Style.RESET_ALL)
+                comment = (Fore.LIGHTYELLOW_EX + f"{input_attempt[index]} Є в слові, але не стоїть на своєму місті" + Style.RESET_ALL)
             else:
                 check.append(Fore.RED + input_attempt[index] + Style.RESET_ALL)
+                comment = (Fore.LIGHTRED_EX + f"{input_attempt[index]} Цієї літери немає в слові" + Style.RESET_ALL)
+            print(comment)
+        print("------------------------------------------------")
         print(' '.join(check))
         if input_attempt == secret:
             print("Вітаю, ви вгадали")
