cfb48e0f90d53d02b71c248294a6804031c2dd8e
volOrobyov <25fmif.v.orobiov@std.udu.edu.ua>
2025-11-26 21:38:19 +0200

    Додав кількість спроб для вгадування слова

--- WORDLE.py
+++ WORDLE.py
@@ -1,5 +1,16 @@
 import random
 print("Ласкаво просимо в гру!")
 
+print("У вас є 6 спроб щоб вгадати слово")
 words = ["apple","house","water","green","happy","table","light","bread","fruit","sleep"]
 words1 = random.choice(words)
+attempts = 0
+while True:
+    if attempts > 6:
+        break
+    else:
+        input_attempt = input("Спробуйте вгадати слово з 5 літер")
+        if len(input_attempt) != 5:
+            print("Ви ввели слово, яке складається більше ніж з 5 літер")
+        elif input_attempt == 5:
+            attempts += 1
